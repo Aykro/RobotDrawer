3a94203361736f13aaafd8e969242563cb2469cd
Marcin Pochrzęst <marcpoc568@student.polsl.pl>
2018-09-01 21:04:22 +0200

    Initial Commit

---
+++ RobotDrawer/RobotDrawer/ViewModels/AboutViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MvvmDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotDrawer.ViewModels
+{
+    class AboutViewModel : ViewModelBase, IModalDialogViewModel
+    {
+        public bool? DialogResult { get { return false; } }
+
+        public string Content
+        {
+            get
+            {
+                return "RobotDrawer" + Environment.NewLine +
+                        "Created by Marcin Pochrzęst" + Environment.NewLine +
+                        "Address" + Environment.NewLine +
+                        "2018";
+            }
+        }
+
+        public string VersionText
+        {
+            get
+            {
+                var version1 = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+                // For external assemblies
+                // var ver2 = typeof(Assembly1.ClassOfAssembly1).Assembly.GetName().Version;
+                // var ver3 = typeof(Assembly2.ClassOfAssembly2).Assembly.GetName().Version;
+
+                return "RobotDrawer v" + version1.ToString();
+            }
+        }
+    }
+}
